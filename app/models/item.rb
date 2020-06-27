cb8da285926bab0a1bd7b01038bcd0b75358d998
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-06-27 16:23:09 +0900

    itemモデルにバリデーションとアソシエーションを作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+
+  # validation
+  validates :name, presence: true
+  validates :price, presence: true
+
+  # association
+  has_many :purchases
+  has_many :images, dependent: :destroy
+  belongs_to :user
+  belongs_to :brand
+  belongs_to :category
 end
