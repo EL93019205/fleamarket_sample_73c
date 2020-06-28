36ab176d177d54fb52b41369a652655bae8d9460
Hiyokokeko <sukepon0914@gmail.com>
2020-06-28 13:43:39 +0900

    ユーザーテーブルにカラムを追加するマイグレーション作成(1)

--- db/migrate/20200627135233_add_column_1_to_user.rb
+++ db/migrate/20200627135233_add_column_1_to_user.rb
@@ -1,4 +1,13 @@
 class AddColumn1ToUser < ActiveRecord::Migration[5.2]
   def change
+    create_table :users do |t|
+      t.string :nickname, null: false
+      t.string :family_name, null: false
+      t.string :first_name, null: false
+      t.string :family_name_kana, null: false
+      t.string :first_name_kana, null: false
+      t.integer :birthday_year, null: false
+      t.integer :birthday_month, null: false
+      t.integer :birthday_day, null: false
   end
 end
