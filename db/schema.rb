3ad756fcc11e082f25e78e97134a079584836906
Hiyokokeko <sukepon0914@gmail.com>
2020-06-28 15:53:53 +0900

    rails db:migrate
    誕生日をdate型に修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_27_135250) do
+ActiveRecord::Schema.define(version: 2020_06_28_064858) do
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "item_id", null: false
@@ -40,9 +40,6 @@ ActiveRecord::Schema.define(version: 2020_06_27_135250) do
     t.string "first_name", null: false
     t.string "family_name_kana", null: false
     t.string "first_name_kana", null: false
-    t.integer "birthday_year", null: false
-    t.integer "birthday_month", null: false
-    t.integer "birthday_day", null: false
     t.string "d_family_name", null: false
     t.string "d_first_name", null: false
     t.string "d_family_name_kana", null: false
@@ -53,6 +50,7 @@ ActiveRecord::Schema.define(version: 2020_06_27_135250) do
     t.string "address", null: false
     t.string "building_name"
     t.string "phone_number", limit: 11
+    t.date "birthday", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
