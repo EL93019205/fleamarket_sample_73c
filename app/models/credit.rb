389c91503cbdfc97d9ab7cd46cc4fd1d0b8b91ae
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-07-03 00:46:24 +0900

    デプロイテスト
    unicornのstderrログを参照するとcredit.rbのvalidatesの改行時にコロンがないと怒られたのでつける

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -4,26 +4,27 @@ class Credit < ApplicationRecord
   has_one :user, dependent: :destroy
 
   # 必須項目(空欄禁止)  
-  validates :card_fullname, :card_number, :expiration_year, \
+  validates :card_fullname, :card_number, :expiration_year,
             :expiration_month, :security_code, :user, presence: true
+
   #属性の値を一意にする
   validates :card_number, :user, uniqueness: true
 
   #クレジットカードの名前
-  validates :card_fullname
+  validates :card_fullname,
     format: { with: /^[A-Z]+\s[A-Z]+\z/, message: 'を半角アルファベット(大文字)で入力してください' }
 
   #クレジットカード番号, 有効期限
-  validates :card_number
-    format: { with: /\A[0-9]+\z/, message: 'を半角数字で入力してください' }
+  validates :card_number,
+    format: { with: /\A[0-9]+\z/, message: 'を半角数字で入力してください' },
     length: { maximum: 16 }
 
   #有効期限(月/年)
-  validates :expiration_month, :expiration_year
+  validates :expiration_month, :expiration_year,
     format: { with: /\A[0-9]+\z/, message: 'を半角数字で入力してください' }
 
   #セキュリティコード
-  validates :security_code
-    format: { with: /\A[0-9]+\z/, message: 'を半角数字で入力してください' }
+  validates :security_code,
+    format: { with: /\A[0-9]+\z/, message: 'を半角数字で入力してください' },
     length: { maximum: 4 }
 end
