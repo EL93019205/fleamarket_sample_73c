9eb14eb017ed70549ed2b18d4fbcc3cb9ad1a60d
Hiyokokeko <sukepon0914@gmail.com>
2020-07-04 17:21:02 +0900

    クレジットカード新規登録ページ・編集ページのフロント作成

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -1,13 +1,36 @@
 class CreditsController < ApplicationController
   def new
+    @user = User.find(params[:user_id])
+    @credit = Credit.new
   end
 
   def create
+    @credit = Credit.new(credit_params)
+    if @credit.save
+      redirect_to edit_user_path(@credit), notice: 'クレジットカード情報を登録しました'
+    else
+      render :new
+    end
   end
- 
+
   def edit
+    @user = User.find(params[:user_id])
+    @credit = Credit.find(params[:user_id])
   end
- 
+
   def update
+    @credit = Credit.find(params[:user_id])
+    if @credit.update(credit_params)
+      redirect_to edit_user_path(@credit), notice: 'クレジットカード情報を更新しました'
+    else
+      render :edit
+    end
   end
+
+  private
+
+  def credit_params
+    params.require(:credit).permit(:card_fullname, :card_number, :expiration, :security_code).merge(user_id: params[:user_id])
+  end
+
 end
