d54f5b3e25370c4c61bc1c4fe5b610c8d206d6fd
KazuyaUmezawa <KazuyaUmezawa1024@gmail.com>
2020-07-04 17:23:39 +0900

    他の人のマイページに飛べない、ログインしてないとマイページにとべない機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :correct_user
   def edit
     # データベースにクレジットデータが存在する場合は@creditに該当データが入る
     # まだなければ@creditにnilが入る
@@ -7,4 +9,12 @@ class UsersController < ApplicationController
       @credit = Credit.find(params[:user])
     end
   end
+
+private
+
+def correct_user
+  user = User.find(params[:id])
+  redirect_to root_path, alert: '他の人のマイページには飛べません' if user.id != current_user.id
+end
+
 end
