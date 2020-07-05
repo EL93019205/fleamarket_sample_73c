46e6d0ac22c838f5fc8f48407289c14d5d49608f
KazuyaUmezawa <KazuyaUmezawa1024@gmail.com>
2020-07-05 16:32:27 +0900

    brandモデルとmigrationファイルの修正

--- db/migrate/20200705065550_create_brands.rb
+++ db/migrate/20200705065550_create_brands.rb
@@ -1,8 +1,8 @@
 class CreateBrands < ActiveRecord::Migration[5.2]
   def change
     create_table :brands do |t|
-      t.string "name", null: false, unique: true, index: true
-      t.string "path", null: false, unique: true
+      t.string :name, null: false, unique: true, index: true
+      t.string :path
       t.timestamps
     end
   end
