d7a9cbc58f305a9cfb8aec6b8b3ae511ff6cab6b
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-07-05 16:35:09 +0900

    アイテムのバリデーション追加し忘れたから追加
    バリデーション追加し忘れたから追加
    あとuserが消えたらitemも消えるように修正(なんか理由があって消した気がするが忘れた)

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,15 @@
 class Item < ApplicationRecord
 
   # validation
-  validates :name, presence: true
-  validates :price, presence: true
+  validates :name, :introduction, :price, 
+    :category, :user, :condition, :trading_status,
+    :shipping_area, :shipping_days, :shipping_price, presence: true
 
   # association
 #  has_many :purchases
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
   belongs_to :brand
   belongs_to :category
 end
