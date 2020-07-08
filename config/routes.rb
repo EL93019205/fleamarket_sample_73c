760625b334e9d64f06cbdf4e4f2ddbf31c7cfa41
Hiyokokeko <sukepon0914@gmail.com>
2020-07-08 23:10:36 +0900

    商品出品の入力エラー時にカテゴリーが選択できなくなる問題の解決
    ・Ajaxで指定するurlに差異が生じる仕様のため解決策としてルーティングの追加
    ・ついでに不要なルーティングの削除

--- config/routes.rb
+++ config/routes.rb
@@ -15,14 +15,12 @@ Rails.application.routes.draw do
 
   root 'items#index'
   resources :items, except: [:show] do
+    # Ajaxで動くアクションのルート
     collection do
       get 'get_category_children', defaults: { format: 'json' }
       get 'get_category_grandchildren', defaults: { format: 'json' }
     end
-    member do
-      get 'get_category_children', defaults: { format: 'json' }
-      get 'get_category_grandchildren', defaults: { format: 'json' }
-    end
   end
-  
+  get 'get_category_children', to: 'items#get_category_children', defaults: { format: 'json' }
+  get 'get_category_grandchildren', to: 'items#get_category_grandchildren', defaults: { format: 'json' }
 end
