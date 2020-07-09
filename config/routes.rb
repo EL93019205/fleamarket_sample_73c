c57d9301ff26589e205c5a1610c00ce92c628834
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-07-10 02:34:22 +0900

    (不具合修正)編集画面でカテゴリが選択できない
    やっぱりmember(idつきパス)は必要でした

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,10 @@ Rails.application.routes.draw do
       get 'get_category_children', defaults: { format: 'json' }
       get 'get_category_grandchildren', defaults: { format: 'json' }
     end
+    member do
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren', defaults: { format: 'json' }
+    end
   end
   get 'get_category_children', to: 'items#get_category_children', defaults: { format: 'json' }
   get 'get_category_grandchildren', to: 'items#get_category_grandchildren', defaults: { format: 'json' }
