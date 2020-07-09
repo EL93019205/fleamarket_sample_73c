c192cff9620cf16e298a5cbec066c8ef3622d8a2
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-07-09 22:00:07 +0900

    (不具合修正)出品した商品を削除するとユーザーまで消える
    商品削除時にユーザーを消えないようにする

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,7 @@ class Item < ApplicationRecord
 #  has_many :purchases
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
-  belongs_to :user, dependent: :destroy
+  belongs_to :user
   belongs_to :category
 
   # 商品の状態
