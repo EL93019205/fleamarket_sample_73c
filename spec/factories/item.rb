234fe647e0a4b06fc69d85d4d29fd9c6ba81a1c3
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-07-11 12:43:47 +0900

    itemモデルのテストコードを作成

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -1,2 +1,17 @@
 FactoryBot.define do
+
+  factory :item do
+    name                  {"シャネルのバッグ 限定品"}
+    introduction          {"こちらの商品は、昔から愛用しており、購入から10年経っております。"}
+    price                 {"100"}
+    brand                 {"シャネル"}
+    condition             {"新品、未使用"}
+    trading_status        {"出品中"}
+    shipping_area         {"愛知県"}
+    shipping_days         {"1〜2日で発送"}
+    shipping_price        {"送料込み(出品者負担)"}
+    association :user
+    association :category
+  end
+
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,1 +1,103 @@
 require 'rails_helper'
+
+describe Item do
+  describe '#create' do
+    context '有効なテスト' do
+      it '全項目の入力' do
+        item = build(:item)
+        item.images.new
+        expect(item).to be_valid
+      end
+      it 'ブランドが入力されていない' do
+        item = build(:item, brand: nil)
+        item.images.new
+        expect(item).to be_valid
+      end
+    end
+    context '無効なテスト' do
+      it '画像が選択されていない' do
+        item = build(:item)
+        item.valid?
+        expect(item.errors[:images]).to include('を最低1枚選択してください')
+      end
+      it '商品名が入力されていない' do
+        item = build(:item, name: nil)
+        item.images.new
+        item.valid?
+        expect(item.errors[:name]).to include('を入力してください')
+      end
+      it '商品説明が入力されていない' do
+        item = build(:item, introduction: nil)
+        item.images.new
+        item.valid?
+        expect(item.errors[:introduction]).to include('を入力してください')
+      end
+      it '値段が入力されていない' do
+        item = build(:item, price: nil)
+        item.images.new
+        item.valid?
+        expect(item.errors[:price]).to include('は半角数字を入力してください')
+      end
+      it '値段が半角数字でない' do
+        item = build(:item, price: "あ")
+        item.images.new
+        item.valid?
+        expect(item.errors[:price]).to include('は半角数字を入力してください')
+      end
+      it '値段が0円以下' do
+        item = build(:item, price: -1)
+        item.images.new
+        item.valid?
+        expect(item.errors[:price]).to include('は0より大きくなければなりません')
+      end
+      it '商品の状態が選択されていない(初期値)' do
+        item = build(:item, condition: "選択してください")
+        item.images.new
+        item.valid?
+        expect(item.errors[:condition]).to include('を選択してください')
+      end
+      it '配送地域が選択されていない(初期値)' do
+        item = build(:item, shipping_area: "選択してください")
+        item.images.new
+        item.valid?
+        expect(item.errors[:shipping_area]).to include('を選択してください')
+      end
+      it '配送日数が選択されていない(初期値)' do
+        item = build(:item, shipping_days: "選択してください")
+        item.images.new
+        item.valid?
+        expect(item.errors[:shipping_days]).to include('を選択してください')
+      end
+      it '配送料が選択されていない(初期値)' do
+        item = build(:item, shipping_price: "選択してください")
+        item.images.new
+        item.valid?
+        expect(item.errors[:shipping_price]).to include('を選択してください')
+      end
+      it '商品の状態が選択されていない(nil)' do
+        item = build(:item, condition: nil)
+        item.images.new
+        item.valid?
+        expect(item.errors[:condition]).to include('を入力してください')
+      end
+      it '配送地域が選択されていない(nil)' do
+        item = build(:item, shipping_area: nil)
+        item.images.new
+        item.valid?
+        expect(item.errors[:shipping_area]).to include('を入力してください')
+      end
+      it '配送日数が選択されていない(nil)' do
+        item = build(:item, shipping_days: nil)
+        item.images.new
+        item.valid?
+        expect(item.errors[:shipping_days]).to include('を入力してください')
+      end
+      it '配送料が選択されていない(nil)' do
+        item = build(:item, shipping_price: nil)
+        item.images.new
+        item.valid?
+        expect(item.errors[:shipping_price]).to include('を入力してください')
+      end
+    end
+  end
+end
